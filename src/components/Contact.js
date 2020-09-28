528164fd5cae2a9f84c3fc20af7dadbf17607707
Ricky199X <rickyrojas19@gmail.com>
2020-09-28 17:58:44 -0400

    some light styling to contact page - outlining general structure

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,16 +4,21 @@ import logo from '../assets/logo.jpg'
 
 const Contact = () => {
     return (
-        <div className="container">
-            <div className="logo">
-                <img src={logo} height='400px' width='550px' />
-            </div>
+        <div className="page-container">
 
-            <div className="address">
+            {/* Contact Page Logo */}
+            <section className="contact-page-logo center">
+                <div className="logo">
+                    <img src={logo} height='400px' width='550px' />
+                </div>
+            </section>
+
+            {/* Address Div */}
+            <section className="box center">
                 <p>41 Beckford Street</p>
                 <p>Beverly, MA 01915</p>
                 <p>(Corner of Beckford and Cabot)</p>
-            </div>
+            </section>
         </div>
     )
 }
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SocialMedia = () => {
+    return (
+        <div>
+            <h1>this is the social media component</h1>
+        </div>
+    )
+}
+
+
+
+export default SocialMedia
