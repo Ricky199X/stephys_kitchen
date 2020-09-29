56c45dc08e07b3ba9006c9509a74871e449ec02d
Ricky199X <rickyrojas19@gmail.com>
2020-09-28 23:21:22 -0400

    changed the background color to whitesmoke

--- src/components/Lunch.js
+++ src/components/Lunch.js
@@ -3,8 +3,10 @@ import lunch from '../assets/lunch.jpg'
 
 const Lunch = () => {
     return (
-        <div className="lunch-menu">
-            <img src={lunch} height='1000px' width='1300px' />
+        <div className="page-container">
+            <div className="lunch-menu">
+                <img src={lunch} height='950px' width='1350px' />
+            </div>
         </div>
     )
 }
