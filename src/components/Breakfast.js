1c9ae48b24e68969a5d070c778f95006644ab663
Ricky199X <rickyrojas19@gmail.com>
2020-09-28 23:13:39 -0400

    fixed padding on breakfast menu + page structure

--- src/components/Breakfast.js
+++ src/components/Breakfast.js
@@ -3,8 +3,10 @@ import breakfast from '../assets/breakfast.jpg'
 
 const Breakfast = () => {
     return (
-        <div className="breakfast-menu">
-            <img src={breakfast} height='1000px' width='1270px' />
+        <div className="page-container">
+            <div className="breakfast-menu">
+                <img src={breakfast} height='950px' width='1350px' />
+            </div>
         </div>
     )
 }
