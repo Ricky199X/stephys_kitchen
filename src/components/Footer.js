fa28cffae623e8411cd509ebf1095efb62653c47
Ricky199X <rickyrojas19@gmail.com>
2020-09-30 18:20:42 -0400

    adds basic structure to the footer + info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,21 @@ import React from 'react'
 
 
 const Footer = () => {
+
+    const fullYear = new Date().getFullYear()
+
     return (
-        <div className="page-footer #1a237e indigo darken-4">
-            <h4>This is a footer</h4>
-        </div>
+        <footer class="page-footer #1a237e indigo darken-4">
+            <div className="row">
+                <div className="col s6">
+                    <h4>Stephy's Kitchen &copy; {fullYear}</h4>
+                </div>
+                <div className="col s6">
+                    <h4>978-921-6393</h4>
+                </div>
+            </div>
+
+        </footer>
     )
 }
 
