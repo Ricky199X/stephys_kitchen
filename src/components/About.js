d2fbddf3b3da9c31a279ca4af11514aebb444961
Ricky199X <rickyrojas19@gmail.com>
2020-10-01 14:44:04 -0400

    fixed padding of the main container

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,9 @@ const About = () => {
         <div class="about-content">
             <p>
                 Stephy's Kitchen is the classic American diner experience! It's all about great food and amazing value.
-                Breakfasts feature eggs, omelettes, crisp bacon, large slices of ham, corned beef hash, steak tips, (Getting hungry yet?) pancakes, all sorts of juices and a great cup of Joe.
-                Lunches are awesome too! burgers, club sandwiches, fries, salads and a huge selection of Val's Daily Specials like fish chowder, boneless stuffed chicken, meatloaf and more!
+                Breakfasts feature eggs, omelettes, crisp bacon, large slices of ham, corned beef hash, steak tips, pancakes, all sorts of juices and a great cup of Joe.
+                Lunches are awesome too! Burgers, club sandwiches, fries, salads and a huge selection of Val's Daily Specials like fish chowder, boneless stuffed chicken, meatloaf and more!
+                Getting hungry yet?
             </p>
         </div>
     )
