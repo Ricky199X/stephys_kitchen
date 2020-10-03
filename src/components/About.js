b5fb9ee243cc344eae377dda50615e52754a43da
Ricky199X <rickyrojas19@gmail.com>
2020-10-03 18:04:46 -0400

    adds flow text attribute to about paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
 
     return (
         <div class="about-content">
-            <p>
+            <p class="flow-text">
                 Stephy's Kitchen is the classic American diner experience! It's all about great food and amazing value.
                 Breakfasts feature eggs, omelettes, crisp bacon, large slices of ham, corned beef hash, steak tips, pancakes, all sorts of juices and a great cup of Joe.
                 Lunches are awesome too! Burgers, club sandwiches, fries, salads and a huge selection of Val's Daily Specials like fish chowder, boneless stuffed chicken, meatloaf and more!
